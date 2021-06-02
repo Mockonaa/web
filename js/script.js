643c2ebf4a7ab198123dee4e004241344b9c2202
Juju <lpomastergbcbien@gmail.com>
2021-06-02 12:27:53 +0200

    vraie refonte x)

---
+++ js/script.js
@@ -0,0 +1,4 @@
+
+
+    window.silex = window.silex || {}
+    window.silex.data = {"site":{"width":1200},"pages":[{"id":"page-index","displayName":"index","link":{"linkType":"LinkTypePage","href":"#!page-index"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-modpack","displayName":"modpack","link":{"linkType":"LinkTypePage","href":"#!page-modpack"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true,"opened":false}]}
